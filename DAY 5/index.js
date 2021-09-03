3d6a1557bd758517f1633f1fb59043da9383f121
saksshham <81289215+saksshham@users.noreply.github.com>
2021-09-03 22:41:15 +0530

    Day 5

--- DAY 5/index.js
+++ DAY 5/index.js
@@ -1,9 +1,7 @@
 var Kilaas = require("./require_test");
 var FC = require("./require_test");
-// We use classes by creating object variables
 var kilaas_obj = new Kilaas();
 var FC_obj = new FC();
 
 console.log(kilaas_obj.print());
-
 console.log(FC_obj.print());
