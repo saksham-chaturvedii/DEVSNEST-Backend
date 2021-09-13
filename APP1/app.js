009bdf93d1de040fbbbbaa4871e3b1ee363f4627
Ubuntu <ubuntu@ip-172-31-86-176.ec2.internal>
2021-09-13 18:57:15 +0000

    too many redirects handled

--- APP1/app.js
+++ APP1/app.js
@@ -12,5 +12,6 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/albums", albumRouter);
 app.use("/download/images", downloadRouter);
+app.use('/', (req, res)=>{res.redirect('/albums');});
 module.exports = app;
 // port-> 1111
