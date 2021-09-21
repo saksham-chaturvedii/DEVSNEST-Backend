cb58923d0b50148d7eed6b55a44bb7b9822c10f1
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-09-21 23:42:16 +0530

    Day 11 restart

--- DAY 11/routes/index.js
+++ DAY 11/routes/index.js
@@ -1,12 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var registerChecks = require("../middlewares/registerChecks");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-
-// router.post('/register', )
 
 module.exports = router;
